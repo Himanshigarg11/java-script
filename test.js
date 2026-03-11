637b0323276f0d9819ee9b2935d8a4b150ccf4d9
himanshi11 <himanshigarg1102@gmail.com>
2026-03-11 18:47:30 +0530

    basics done

---
+++ test.js
@@ -0,0 +1,22 @@
+const accountId=144553
+let accountEmail="himanshigarg1102@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+// accountId=2 not allowed
+accountEmail="hhblng@gmail.com"
+accountPassword="2323212"
+accountCity="bengalurur"
+let accountState;
+{
+   let accountPassword="1234565432"
+   console.log(accountPassword);
+}
+console.log(accountId);
+console.table([accountId,accountEmail,
+    accountPassword,accountCity, accountState])
+
+
+    const name="himanshi"
+    const age="20"
+    console.log(`hey sp my name is ${name} and my age is ${age}`)
