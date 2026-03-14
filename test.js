1a3687f2d9b483f697a9052dc35f3c27babee129
himanshi11 <himanshigarg1102@gmail.com>
2026-03-14 20:17:26 +0530

    Dom manipulation

--- test.js
+++ test.js
@@ -1,22 +1,71 @@
-const accountId=144553
-let accountEmail="himanshigarg1102@gmail.com"
-var accountPassword="12345"
-accountCity="jaipur"
+// const accountId=144553
+// let accountEmail="himanshigarg1102@gmail.com"
+// var accountPassword="12345"
+// accountCity="jaipur"
 
-// accountId=2 not allowed
-accountEmail="hhblng@gmail.com"
-accountPassword="2323212"
-accountCity="bengalurur"
-let accountState;
-{
-   let accountPassword="1234565432"
-   console.log(accountPassword);
+// // accountId=2 not allowed
+// accountEmail="hhblng@gmail.com"
+// accountPassword="2323212"
+// accountCity="bengalurur"
+// let accountState;
+// {
+//    let accountPassword="1234565432"
+//    console.log(accountPassword);
+// }
+// console.log(accountId);
+// console.table([accountId,accountEmail,
+//     accountPassword,accountCity, accountState])
+
+
+//     const name="himanshi"
+//     const age="20"
+
+//     console.log(typeof name)
+   //  const hey=new String("imanshi");
+   //   console.log(typeof hey)
+   //   hey=hey.toUpperCase;
+   //   console.log(hey);
+//  const mysyn=Symbol("key1")
+//    const obj={
+//       name:"himanshi",
+//       mysyn:"mykey1",
+//       age:18
+//    }
+//    console.log(typeof obj["mysyn"]);
+
+// const course={
+//    CourseName:"java script",
+//    price:1000,
+//    CourseOwner:"himanshi"
+// }
+
+// const {CourseOwner}=course  
+// // course -----> kahan sa value extract krni that is course obj
+// //in bracket u will tell consi value extract krni 
+// console.log(CourseOwner);
+// console.log(course.CourseOwner);
+// const {CourseOwner: owner}=course
+// // u can also do like this after ":" give some short name 
+// console.log(owner); 
+
+function sayName(){
+   console.log("H");
+   console.log("I");
+   console.log("M");
+   console.log("A");
+   console.log("N");
+   console.log("S");
+   console.log("H");
+   console.log("I");
 }
-console.log(accountId);
-console.table([accountId,accountEmail,
-    accountPassword,accountCity, accountState])
+sayName();//----> this is function execution
+//sayNmae ---> this is function refrence 
+// for execution u have to pass parantheses
+//{ } this is function scope and can say function defination 
 
 
-    const name="himanshi"
-    const age="20"
-    console.log(`hey sp my name is ${name} and my age is ${age}`)
+function addTwoNumbers(num1, num2){
+   console.log(num1+num2);
+}
+addTwoNumbers(2,5)
+
