c091ef58b7f6bc440db9399ebcaa5aa9993a09a8
himanshi11 <himanshigarg1102@gmail.com>
2026-03-22 22:32:18 +0530

    complete java script learning

--- prototype/index.js
+++ prototype/index.js
@@ -26,4 +26,35 @@
 // user1.printME()
 
 
+// const heror=['spider','thor']
+// const heroPower={
+//     thor: "hammer",
+//     spiderman: "sling",
+//     getSpiderPower: function(){
+//         console.log(`spidy power is ${this.spiderman}`)
+//     }
+
+// }
+
+// Object.prototype.hitesh=function(){
+//     console.log('hitesh is present in all objects')
+// }
+// Array.prototype.himanshi=function(){
+//     console.log('himanshi is present in all arrays')
+// }
+//  heroPower.hitesh()
+//  heror.hitesh()
+
+//  heror.himanshi()
+// heroPower.himanshi()
+
+let user1="chaiaurcode       "
+let  user2="    himaaa    "
+String.prototype.trueLength=function(){
+    console.log(this)
+    console.log(`true length is: ${this.trim().length}`)
+}
+
+user1.trueLength()
+user2.trueLength()
 
